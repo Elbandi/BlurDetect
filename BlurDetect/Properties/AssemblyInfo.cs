58eb8f786f17f91c7fe415912e6763bca66b6bf7
Andras Elso <elso.andras@gmail.com>
2020-09-13 15:07:42 +0200

    Fill Assemblyinfo

--- BlurDetect/Properties/AssemblyInfo.cs
+++ BlurDetect/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BlurDetect")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Detect image blur value and show histogram")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elbandi.Net")]
 [assembly: AssemblyProduct("BlurDetect")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
